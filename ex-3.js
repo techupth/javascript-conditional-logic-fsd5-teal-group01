04a41f99248a90d5cb126ef6e9e34d3afafcbb9e
Sodikcp <paramesh.rah@gmail.com>
2023-11-30 14:28:29 +0700

    feat:complete exercise logic

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,9 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+let message=
+lightBulbStatus == "On"? "Light bulb is On."
+:lightBulbStatus == "Off"? "Light bulb is Off."
+:"Please choose the correct input (On/Off)"
+
+console.log(message)
