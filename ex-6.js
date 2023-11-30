959a736396148da6d376464ce152b2dbac4aafa5
Name <pobparis.bun@gmail.com>
2023-11-30 14:55:24 +0700

    ex finish

--- ex-6.js
+++ ex-6.js
@@ -1,10 +1,11 @@
 let favoriteBooks = [];
 
 function addFavoriteBook(bookName) {
-  // Start coding here !
+  if (bookName.includes("Beauty")) {
+    favoriteBooks.push(bookName);
+  }
 }
 
-// Example case
 addFavoriteBook("Let Me Tell You What I Mean");
 addFavoriteBook("Life Among the Terranauts");
 addFavoriteBook("The Beauty of Living Twice");
